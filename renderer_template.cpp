ff8b2ed0b78a9f85c475fc9d1e24b3446d29c200
taniak24 <102926746+taniak24@users.noreply.github.com>
2023-12-18 11:30:38 +0200

    Update renderer_template.cpp

--- renderer_template.cpp
+++ renderer_template.cpp
@@ -1,27 +1,53 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-///BEFORE EVERY PROJECT YOU NEED TO DO THE INSTALLATION STEPS
-
 using namespace std;
 
-const int WIDTH = 500;
-const int HEIGHT = 400;
+const int WIDTH = 900;
+const int HEIGHT = 1500;
 
 int main(int args, char *argv[])
 {
+    //for the image
+    SDL_Surface *imageSurface = NULL;
+    SDL_Surface *windowSurface = NULL;
+
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("SDL WORLD", 500, 500, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window *window = SDL_CreateWindow("First SDL", 10, 10, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 
-    //check in console if it's correctly installed
-    if(window == NULL)
+    windowSurface = SDL_GetWindowSurface(window);
+
+    //check
+    if(NULL == window)
     {
         cout << "could not create a window";
     }
     else
     {
         cout << "SDL is installed";
+    }
+
+    //create the renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    //set the background color
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+    SDL_RenderClear(renderer); //the function clears the entire rendering target
+    SDL_RenderPresent(renderer); //presents the color
+
+    SDL_Delay(3000); //Sleep
+    //set the new color
+    SDL_SetRenderDrawColor(renderer, 255, 0, 225, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    //image
+    imageSurface = SDL_LoadBMP("images/birds.bmp");
+
+    if(imageSurface == NULL)
+    {
+        cout << "not loaded image";
     }
 
     SDL_Event windowEvent;
@@ -35,5 +61,15 @@ int main(int args, char *argv[])
                 break;
             }
         }
+         SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+         SDL_UpdateWindowSurface(window);
     }
+    SDL_FreeSurface(imageSurface);
+    SDL_FreeSurface(windowSurface);
+
+    imageSurface = NULL;
+    windowSurface = NULL;
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
